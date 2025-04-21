96768b2c0b45e67977402219940d290bc37b7c20
Phong Do <Donguyenphong@icloud.com>
2025-04-20 18:17:00 -0700

    Basic Structure done

--- src/main.c
+++ src/main.c
@@ -1,6 +1,40 @@
+#include <SDL.h>
+#include <SDL_vulkan.h>
 #include <stdio.h>
+#include <vulkan/vulkan.h>
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
 int main() {
-  printf("Hello, world!\n");
+  SDL_Window *window = NULL;
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    return 1;
+  }
+
+  window = SDL_CreateWindow(
+      "orbgen", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+      SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN);
+
+  if (window == NULL) {
+    SDL_Quit();
+    return 1;
+  }
+
+  int quit = 0;
+  SDL_Event e;
+
+  while (!quit) {
+    while (SDL_PollEvent(&e) != 0) {
+      if (e.type == SDL_QUIT) {
+        quit = 1;
+      }
+    }
+  }
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
   return 0;
 }
